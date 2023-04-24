07e90745b828adb914f000a5c543b8aa0c7f966c
Uliana Le <ulianochka29@mail.ru>
2023-04-24 10:24:25 +0300

    add changes and new file

--- cypress2.config.js
+++ cypress2.config.js
@@ -2,14 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
-    retries: {
-      openMode: 1,
-      },
+    baseUrl: "http://localhost:3000",
+    // retries: 2,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080,
+    viewportWidth: 414,
+    viewportHeight: 896,
   },
 });
